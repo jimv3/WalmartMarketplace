aa6e964b5a20e10d08fcf2fcf3fcfa6437664e24
Vaughan, Jim <jim.vaughan@ingramcontent.com>
2023-12-27 15:37:52 -0600

    Initial comit

---
+++ retireitems.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const readline = require('readline')
+
+async function processLineByLine(fileName) {
+    const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    const fileStream = fs.createReadStream(fileName)
+    const todaysDate = new Date(Date.now())
+    const year = todaysDate.getFullYear()
+    const month = months[todaysDate.getMonth()]
+    const day = todaysDate.getDate().toString().padStart(2, "0")
+    const hour = todaysDate.getHours().toString().padStart(2, "0")
+    const minute = todaysDate.getMinutes().toString().padStart(2, "0")
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    })
+
+    let fileNumber = 1
+    let fileInfo = {
+        "RetireItemHeader": {
+            "feedDate": todaysDate,
+            "version": "1.0"
+        }
+    }
+    let items = []
+    for await (const line of rl) {
+        items.push({ "sku": line })
+
+        if (items.length === 10000) {
+            fileInfo["RetireItem"] = items
+            console.log(`File: ${fileNumber}`)
+            //console.log(JSON.stringify(fileInfo))
+            fs.writeFileSync(`/Users/jvaugha/RetireItems/retire_items_${year}${month}${day}_${hour}${minute}_${fileNumber}.json`, JSON.stringify(fileInfo))
+            items = []
+            fileNumber += 1
+        }
+    }
+
+    if (items.length > 0) {
+        fileInfo["RetireItem"] = items
+        fs.writeFileSync(`/Users/jvaugha/RetireItems/retire_items_${year}${month}${day}_${hour}${minute}_${fileNumber}.json`, JSON.stringify(fileInfo))
+    }
+}
+
+
+if (process.argv.length < 3) {
+    processLineByLine('/Users/jvaugha/Downloads/RetireItems.csv')
+}
+else {
+    processLineByLine(process.argv[2])
+}
